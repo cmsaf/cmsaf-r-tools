a8be5f1e676d53e255896a3f3a90419b6615a72e
Kothe Steffen <stkothe@omlws18.dwd.de>
2022-08-15 15:53:36 +0200

    Finalise cmsaf 3.4.4 with fixed bug.

--- cmsaf/inst/toolbox/ui.R
+++ cmsaf/inst/toolbox/ui.R
@@ -9,7 +9,7 @@
 descriptionString <-
   "
 
-The CM SAF R TOOLBOX 3.4.3 -- 'Just Read the Instructions'
+The CM SAF R TOOLBOX 3.4.4 -- 'Just Read the Instructions'
 
 The intention of the CM SAF R Toolbox is to help you using
 CM SAF NetCDF formatted climate data
@@ -25,7 +25,7 @@ right in and analyze or visualize a .nc file.
 Suggestions for improvements and praise for the developers
 can be sent to contact.cmsaf@dwd.de.
 
-- Steffen Kothe - 2022-03-16 -"
+- Steffen Kothe - 2022-08-15 -"
 
 # Variable can be found in global.R
 if (isRunningLocally) {
